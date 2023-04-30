68f2a757979d4a1230b4007fb97c0c1cc8b931a2
gabeud838 <102391103+gabeud838@users.noreply.github.com>
2023-04-30 16:46:10 -0300

    Add files via upload

---
+++ assets/preload/custom_events/DodgeEvent.lua
@@ -0,0 +1,56 @@
+function onCreate()
+    --variables
+	Dodged = false;
+    canDodge = false;
+    DodgeTime = 0;
+	
+    precacheImage('spacebar');
+    precacheSound('DODGE');
+	precacheSound('Dodged');
+end
+
+function onEvent(name, value1, value2)
+    if name == "DodgeEvent" then
+    --Get Dodge time
+    DodgeTime = (value1);
+	
+    --Make Dodge Sprite
+	makeAnimatedLuaSprite('spacebar', 'spacebar', 400, 200);
+    luaSpriteAddAnimationByPrefix('spacebar', 'spacebar', 'spacebar', 25, true);
+	luaSpritePlayAnimation('spacebar', 'spacebar');
+	setObjectCamera('spacebar', 'other');
+	scaleLuaSprite('spacebar', 0.50, 0.50); 
+    updateHitbox('spacebar')
+    screenCenter('spacebar')
+    addLuaSprite('spacebar', true); 
+	
+	--Set values so you can dodge
+    playSound('DODGE', 0.3);
+	canDodge = true;
+	runTimer('Died', DodgeTime);
+	
+	end
+end
+
+function onUpdate()
+   if canDodge == true and keyJustPressed('space') then
+   
+   Dodged = true;
+   playSound('Dodged', 0.5);
+   characterPlayAnim('boyfriend', 'dodge', true);
+   setProperty('boyfriend.specialAnim', true);
+   removeLuaSprite('spacebar');
+   canDodge = false
+   
+   end
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+   if tag == 'Died' and Dodged == false then
+   setProperty('health', 0);
+   
+   elseif tag == 'Died' and Dodged == true then
+   Dodged = false
+   
+   end
+end
